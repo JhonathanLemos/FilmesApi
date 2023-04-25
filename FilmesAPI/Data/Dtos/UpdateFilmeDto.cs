beb373322ba7815177750b2203b3264dd7e03652
JhonathanLemos <lemosjhonathan81@gmail.com>
2023-04-25 15:13:47 -0300

    base commit

---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class UpdateFilmeDto
+{
+    [Required(ErrorMessage = "O titulo do filme é obrigatório")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
+    [StringLength(50, ErrorMessage = "O gênero do filme não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600  minutos")]
+    public int Duracao { get; set; }
+}
